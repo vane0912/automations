abec1f19c5bfc96d4c12f35904967135b7ca23df
Vanessa Gonzalez <vanessagpro5@gmail.com>
2024-06-07 15:33:12 -0600

    file changes

--- automations/__init__.py
+++ automations/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from automations import homepage, weekly, applications
 
-
-if __name__ == "__main__":
+def create_app():
     app = Flask(__name__)
-    app.run(debug=True)
+    
+    app.register_blueprint(homepage.bp)
+    app.register_blueprint(weekly.bp)
+    app.register_blueprint(applications.bp)
 
-import automations.homepage
-import automations.weekly
-import automations.applications
+    
+    return app
