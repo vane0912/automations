092860f79f942273fc330bbe230b20d9160eb6f4
Vanessa Gonzalez <vanessagpro5@gmail.com>
2024-06-07 15:26:39 -0600

    railway changes

--- automations/__init__.py
+++ automations/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    app.run(debug=True)
 
 import automations.homepage
 import automations.weekly
