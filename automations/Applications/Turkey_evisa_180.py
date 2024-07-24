a11b49549b4f67fec456fed27f0f2de6bde3f8c8
Vanessa Gonzalez <vanessagpro5@gmail.com>
2024-07-24 15:24:58 -0600

    docker

--- automations/Applications/Turkey_evisa_180.py
+++ automations/Applications/Turkey_evisa_180.py
@@ -1,4 +1,3 @@
-from .imports import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -10,6 +9,37 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import Select
+from .Global_Variables import Global_Variables
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import logging, time, re, requests,json, traceback, os
+def setArguments(data):
+    for x in data:
+        if x['type'] == 'ULR':
+            Global_Variables['url'] = x['value']
+        if x['type'] == 'Email':
+            Global_Variables['Email'] = x['value']
+        if x['type'] == 'Applicants':
+            Global_Variables['applicants'] = x['value']
+        if x['type'] == 'N. Orders':
+            Global_Variables['N. Orders'] = x['value']
+        if x['type'] == 'Status':
+            Global_Variables['Status'] = x['value']
+
+def safe_element_click(driver, locator):
+    attempts = 0
+    max_attempts = 3  
+    while attempts < max_attempts:
+        try:
+            element = WebDriverWait(driver, 60).until(EC.element_to_be_clickable(locator))
+            element.click()
+            return True  # Click successful, return True
+        except EC.StaleElementReferenceException:
+            print(f"StaleElementReferenceException occurred, retrying attempt {attempts + 1}")
+            attempts += 1
+    print(f"Failed to click element after {max_attempts} attempts")
+    return False 
+
 logging.basicConfig(level=logging.ERROR)
 
 console_handler = logging.StreamHandler()
