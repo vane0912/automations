26f2023bfd1f8454b9fa1b29f61d4e92e0b858bc
Vanessa Gonzalez <vanessagpro5@gmail.com>
2024-07-24 14:14:13 -0600

    docker changes

--- automations/Applications/imports.py
+++ automations/Applications/imports.py
@@ -1,15 +1,3 @@
-import logging, time, re, requests,json, traceback, os
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, ElementClickInterceptedException, TimeoutException, ElementNotInteractableException
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.support.ui import Select
 from .Global_Variables import Global_Variables
 
 def setArguments(data):
