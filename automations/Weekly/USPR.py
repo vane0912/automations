989c1c33b3d952bd4fd164dc24b2408810f9cf95
Vanessa Gonzalez <vanessagpro5@gmail.com>
2024-08-24 16:29:31 -0600

    remove headless

--- automations/Weekly/USPR.py
+++ automations/Weekly/USPR.py
@@ -4,7 +4,7 @@ from ..imports import *
 def USPR_PASSPORT_RENEWAL(url, email):
     Order_numbers = []
     chrome_options = Options()
-    #chrome_options.add_argument('--headless') 
+    chrome_options.add_argument('--headless') 
     chrome_options.add_argument('window-size=1920,1080')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
